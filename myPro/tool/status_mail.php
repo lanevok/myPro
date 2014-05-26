8f0f1a828094964bf77f6e96e0566ec70db4d5ae
lanevok <web@lanevok.com>
2014-05-27 05:49:40 +0900

    status_mail(last)

--- myPro/tool/status_mail.php
+++ myPro/tool/status_mail.php
@@ -6,14 +6,29 @@ $response = file_get_contents('http://google.com/', false, $context);
 $pos = strpos($http_response_header[0], '200');
 if ($pos === false) {
   echo 'offline';
-  mb_language("Ja") ;
-  mb_internal_encoding("EUC-JP") ;
-  $mailto="abc@lanevok.com";
-  $subject="title";
-  $content="text";
-  $mailfrom="From:" .mb_encode_mimeheader("def@lanevok.com") ."<def@lanevok.com>";
-  mb_send_mail($mailto,$subject,$content,$mailfrom);
+  $input = file_get_contents("last.txt");
+  if($input==="online"){
+    mb_language("Ja") ;
+    mb_internal_encoding("EUC-JP") ;
+    $mailto="abc@lanevok.com";
+    $subject="offline";
+    $content="text";
+    $mailfrom="From:" .mb_encode_mimeheader("def@lanevok.com") ."<def@lanevok.com>";
+    mb_send_mail($mailto,$subject,$content,$mailfrom);
+    file_put_contents("last.txt","offline");
+  }
 }
 else{
   echo 'online';
+  $input = file_get_contents("last.txt");
+  if($input=="offline"){
+    mb_language("Ja") ;
+    mb_internal_encoding("EUC-JP") ;
+    $mailto="abc@lanevok.com";
+    $subject="online";
+    $content="text";
+    $mailfrom="From:" .mb_encode_mimeheader("def@lanevok.com") ."<def@lanevok.com>";
+    mb_send_mail($mailto,$subject,$content,$mailfrom);
+    file_put_contents("last.txt","online");
+  }
 }
