ab7aef808874f44825fc9ec506e362ae255c8b5c
lanevok <web@lanevok.com>
2014-07-05 20:22:42 +0900

    AC

--- myPro/AtCoder/KUPC2014_B.java
+++ myPro/AtCoder/KUPC2014_B.java
@@ -7,14 +7,14 @@ public class KUPC2014_B {
 
 	boolean[] impossible = new boolean[1001];
 	impossible[0] = true;
-	//		impossible[1] = true;
+	// impossible[1] = true;
 	int request = 1;
 	while(true){
 	    int cnt = 0;
 	    for(int i=0;i<impossible.length;i++){
 		if(!impossible[i]) cnt++;
 	    }
-	    //			System.out.println("残り"+cnt);
+	    // System.out.println("残り"+cnt);
 	    if(cnt==1) break;
 	    for(int i=request+1;i<impossible.length;i++){
 		if(!impossible[i]){
@@ -27,7 +27,7 @@ public class KUPC2014_B {
 	    if(response.contains("N")){
 		for(int i=0;i<impossible.length;i++){
 		    if(!impossible[i]&&i%request==0){
-			//						System.out.println(i+"が割り切れてしまい答えじゃないことが判明した");
+			// System.out.println(i+"が割り切れてしまい答えじゃないことが判明した");
 			impossible[i] = true;
 		    }
 		}
@@ -35,7 +35,7 @@ public class KUPC2014_B {
 	    else if(response.contains("Y")){
 		for(int i=0;i<impossible.length;i++){
 		    if(!impossible[i]&&i%request!=0){
-			//						System.out.println(i+"が割り切れないから答えじゃないことが判明した");
+			// System.out.println(i+"が割り切れないから答えじゃないことが判明した");
 			impossible[i] = true;
 		    }
 		}
