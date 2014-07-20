e3ddbb29ddcf966c9284f47dbfc7aaaec56e5223
lanevok <web@lanevok.com>
2014-07-21 06:57:00 +0900

    TLE

--- myPro/POJ/POJ2919.java
+++ myPro/POJ/POJ2919.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -9,7 +9,6 @@ import java.util.Scanner;
  *
 
  テスト用(ナイトは2以上である)
- 正答4
  1
  ...P....
  ...P..Q.
@@ -19,6 +18,7 @@ import java.util.Scanner;
  ...P....
  .B.P....
  ........
+ 正答4
 
  Sample Input 1
  1
@@ -32,6 +32,7 @@ import java.util.Scanner;
  ........
  正答4
 
+
  Sample Input 2
  1
  B.P.....
@@ -43,6 +44,7 @@ import java.util.Scanner;
  ........
  ........
  正答Imp
+
 
  ノーマル
  1
@@ -56,6 +58,42 @@ import java.util.Scanner;
  +++P++++
  正答5
 
+ ノーマル
+ 1
+ N++++P++
+ +++++PQ+
+ +++++P++
+ +++N+N++
+ +++P+P++
+ +++P+P++
+ +B+P+N++
+ +++P++++
+ 正答5
+
+ ノーマル
+ 1
+ N+++++++
+ ++++++Q+
+ ++++++++
+ +++N+N++
+ ++++++++
+ ++++++++
+ +B+++N++
+ ++++++++
+ 正答4
+
+ ナイトとなり
+ 1
+ +++++P++
+ +++++PQ+
+ +++++P++
+ +++P+N+N
+ +++P+N+N
+ +++P+P++
+ +++P+P++
+ +++P+B++
+ 正答2
+
  ナイト囲み
  1
  N++++P++
@@ -67,6 +105,7 @@ import java.util.Scanner;
  +B+P+N++
  +++P++++
  正答Imp
+
 
  クイーン囲み
  1
@@ -80,6 +119,7 @@ import java.util.Scanner;
  +++P++++
  正答Imp
 
+
  ノーマル2
  1
  N++++P+Q
@@ -91,6 +131,7 @@ import java.util.Scanner;
  +++P++++
  B++P+++N
  正答6
+
 
  全部囲い
  1
@@ -104,6 +145,7 @@ import java.util.Scanner;
  B++P+++N
  正答Imp
 
+
  ポーンなし
  1
  N++++++Q
@@ -116,318 +158,104 @@ import java.util.Scanner;
  B++++++N
  正答3
 
- ナイト11個
+ 近接part1
  1
  N++++++Q
- ++++++++
+ ++++++P+
+ ++++++P+
+ ++++++P+
+ ++++++P+
+ +++++PP+
+ +++++PP+
+ +B+++PN+
+ 正答4
+
+ 近接part2
+ 1
+ N++++++Q
+ ++++++P+
+ ++++++P+
+ ++++++P+
+ ++++++P+
+ +++++PP+
+ +++++PN+
+ +B+++PN+
+ 正答4
+
+ ナイト14個
+ 1
+ N+PN+N+Q
+ ++NP++++
  +++++++N
  +N+N+N++
  ++++++++
  +N+N++N+
  ++++++++
  B+N+N++N
+ 正答8
+ (2秒でも間に合わない可能性)
+ h8d4a7b6g6e8e2g2b2 ?
+
 
  *
  */
-public class POJ2919 {
+public class CopyOfPOJ2919p2 {
 
-    class Point{
-	int x;
-	int y;
+    class Point implements Comparable<Point>{
+	int x, y;
+	String beforeStr;
+
 	Point(int _x, int _y){
 	    x = _x;
 	    y = _y;
+	    beforeStr = "";
 	}
 
-	boolean contain(List<Point> neighboor){
-	    for(Point p : neighboor){
-		if(x==p.x&&y==p.y) return true;
-	    }
-	    return false;
+	int getIndex(){
+	    return x*8+y;
 	}
 
 	@Override
 	    public String toString() {
-	    return "("+x+","+y+")";
+	    String[] c = {"a","b","c","d","e","f","g","h"};
+	    return "("+c[y]+(8-x)+")";
 	}
 
-	public boolean isFree(){
-	    if(!inMat(x, y)||field[x][y]<-15){
-		return false;
-	    }
-	    return true;
+	@Override
+	    public int compareTo(Point o) {
+	    return (((8-x)+y*8)-((8-o.x)+o.y*8));
+	}
+
+	public void append(String str){
+	    beforeStr += str;
+	}
+
+	public String getStr() {
+	    String[] c = {"a","b","c","d","e","f","g","h"};
+	    return c[y]+(8-x);
 	}
     }
 
     int knightNum;
     final int MAT = 8;
+    final int MAX_VALUE = 20000;
     int[][] field;
-    int[][] opt;
+    String[][] memo;
+    String[][] memoBFS;
     List<Point> neighboorKnight;
     List<Point> neighboorBishop;
-
     Point start;
-
     final int PAWN = -40;
     final int QUEEN = -10;
     final int KNIGHT = -20;
     final int BISHOP = -30;
 
     public static void main(String[] args) {
-	new POJ2919().run();
+	new CopyOfPOJ2919p2().run();
     }
 
     private void run() {
-	input();
-    }
-
-    private void output() {
-	// TODO debug
-	//		System.out.println("===== output =====");
-
-	int min = Integer.MAX_VALUE;
-	for(int i=0;i<neighboorKnight.size();i++){
-	    Point nowS = neighboorKnight.get(i);
-	    if(!inMat(nowS.x, nowS.y)) continue;
-	    for(int j=0;j<neighboorBishop.size();j++){
-		Point nowG = neighboorBishop.get(j);
-		if(!inMat(nowG.x, nowG.y)) continue;
-		//				System.out.println(opt[(1<<knightNum)-1][nowS.x*8+nowS.y]);
-		min = Math.min(min, opt[(1<<knightNum)-1][nowS.x*8+nowS.y]+bfs(nowS, nowG));
-	    }
-	}
-	if(min>=10000){
-	    System.out.println("impossible");
-	}
-	else{
-	    System.out.println(min);
-	}
-    }
-
-    private boolean inMat(int i, int j){
-	if(i>=0&&i<MAT&&j>=0&&j<MAT) return true;
-	else return false;
-    }
-
-    private void standby() {
-	Queue<Point> q = new LinkedList<Point>();
-	for(int i=0;i<MAT;i++){
-	    for(int j=0;j<MAT;j++){
-		if(field[i][j]==KNIGHT){
-		    for(int x=-1;x<2;x++){
-			for(int y=-1;y<2;y++){
-			    if(inMat(i+x, j+y)&&!(x==0&&y==0)){
-				q.add(new Point(i+x, j+y));
-			    }
-			    else{
-				if(x==0&&y==0) continue;
-				q.add(new Point(-1,-1));
-			    }
-			}
-		    }
-		}
-	    }
-	}
-	neighboorKnight = new ArrayList<Point>();
-	for(Point p : q){
-	    neighboorKnight.add(p);
-	}
-
-	Queue<Point> q2 = new LinkedList<Point>();
-	for(int i=0;i<MAT;i++){
-	    for(int j=0;j<MAT;j++){
-		if(field[i][j]==BISHOP){
-		    for(int x=-1;x<2;x++){
-			for(int y=-1;y<2;y++){
-			    if(inMat(i+x, j+y)&&!(x==0&&y==0)){
-				q2.add(new Point(i+x, j+y));
-			    }
-			    else{
-				if(x==0&&y==0) continue;
-				q.add(new Point(-1,-1));
-			    }
-			}
-		    }
-		}
-	    }
-	}
-	neighboorBishop = new ArrayList<Point>();
-	for(Point p : q2){
-	    neighboorBishop.add(p);
-	}
-    }
-
-    private void dp() {
-	opt = new int[1<<knightNum][MAT*MAT];
-	for(int i=0;i<(1<<knightNum);i++){
-	    Arrays.fill(opt[i], 10000);
-	}
-
-	// TODO debug
-	//		System.out.println("=== calc 0 ===");
-
-	for(int i=0;i<MAT*MAT;i++){
-	    Point now = new Point(i/MAT, i%MAT);
-	    if(!now.contain(neighboorKnight)&&field[now.x][now.y]>-15){
-		opt[0][i] = bfs(start, new Point(i/MAT, i%MAT));
-	    }
-	}
-
-	// TODO debug
-	//		System.out.println("=== calc 1 ===");
-
-	for(int m=0;m<(1<<knightNum);m++){
-	    //			System.out.println("m="+m);
-	    if(Integer.bitCount(m)==1){
-		for(int i=0;i<neighboorKnight.size();i++){
-		    //					System.err.println(neighboorKnight.size());
-		    //					System.out.println("nei="+i);
-
-		    if(Integer.bitCount((i/8+1)&m)==1){
-			Point temp = neighboorKnight.get(i);
-			if(!inMat(temp.x, temp.y)||field[temp.x][temp.y]<-15) continue;
-			int nowPattern = temp.x*8+temp.y;
-			opt[m][nowPattern] = bfs(start, temp);
-
-			// TODO debug
-			//						System.out.println("opt["+Integer.toBinaryString(m)+"]["+convert(nowPattern)+"]="+opt[m][nowPattern]);
-		    }
-		}
-	    }
-	}
-
-	for(int acKnightNum=2;acKnightNum<=knightNum;acKnightNum++){
-	    // TODO debug
-	    System.out.println("=== calc "+acKnightNum+" ===");
-
-	    for(int nowKnightPattern=1;nowKnightPattern<(1<<knightNum);nowKnightPattern++){
-		if(Integer.bitCount(nowKnightPattern)==acKnightNum){
-		    for(int nowAcKnight=0;nowAcKnight<knightNum;nowAcKnight++){
-			int neighboor_k_Knight_start = nowAcKnight*8;
-			int neighboor_k_Knight_goal = nowAcKnight*8+8;
-			for(int m=neighboor_k_Knight_start;m<neighboor_k_Knight_goal;m++){
-			    Point optPoint = neighboorKnight.get(m);
-			    if(optPoint.isFree()){
-				int optPointPattern = optPoint.x*8+optPoint.y;
-				for(int q=0;q<neighboorKnight.size();q++){
-				    Point basePoint = neighboorKnight.get(q);
-				    if(basePoint.isFree()){
-					int basePointPattern = basePoint.x*8+basePoint.y;
-					int baseKnightPattern = nowKnightPattern&(~(1<<(nowAcKnight)));
-					if(q/8!=nowAcKnight){
-					    // TODO debug
-					    //											System.out.print("baseKnightPattern="+Integer.toBinaryString(baseKnightPattern)+" basePointPattern="+convert(basePointPattern));
-
-					    //											System.out.print(nowKnightPattern);
-					    //											System.out.print(optPointPattern);
-
-					    // TODO debug
-					    //											System.out.print(" opt["+Integer.toBinaryString(nowKnightPattern)+"]["+convert(optPointPattern)+"] = "+opt[nowKnightPattern][optPointPattern]+" => ");
-
-					    opt[nowKnightPattern][optPointPattern] = Math.min(opt[nowKnightPattern][optPointPattern],
-											      opt[baseKnightPattern][basePointPattern]+bfs(basePoint, optPoint));
-
-					    // TODO debug
-					    //											System.out.println(opt[nowKnightPattern][optPointPattern]);
-					}
-				    }
-				}
-			    }
-			}
-		    }
-		}
-	    }
-	}
-    }
-
-    private String convert(int nowPattern) {
-	return "("+(nowPattern/8)+","+(nowPattern%8)+")";
-    }
-
-    /**
-     * locSから1ステップで移動できる候補の座標リストを返す
-     * @param locS 現在の地点
-     * @return 移動できる座標のリスト
-     */
-    private List<Point> possibleMove(Point locS){
-	List<Point> result = new ArrayList<Point>();
-
-	for(int i=locS.x-1;i>=0;i--){
-	    if(field[i][locS.y]>-15) result.add(new Point(i, locS.y));
-	    else break;
-	}
-	for(int i=locS.x+1;i<MAT;i++){
-	    if(field[i][locS.y]>-15) result.add(new Point(i, locS.y));
-	    else break;
-	}
-	for(int i=locS.y-1;i>=0;i--){
-	    if(field[locS.x][i]>-15) result.add(new Point(locS.x, i));
-	    else break;
-	}
-	for(int i=locS.y+1;i<MAT;i++){
-	    if(field[locS.x][i]>-15) result.add(new Point(locS.x, i));
-	    else break;
-	}
-	for(int i=1;i<MAT;i++){
-	    if(inMat(locS.x+i, locS.y+i)&&field[locS.x+i][locS.y+i]>-15) result.add(new Point(locS.x+i, locS.y+i));
-	    else break;
-	}
-	for(int i=1;i<MAT;i++){
-	    if(inMat(locS.x+i, locS.y-i)&&field[locS.x+i][locS.y-i]>-15) result.add(new Point(locS.x+i, locS.y-i));
-	    else break;
-	}
-	for(int i=1;i<MAT;i++){
-	    if(inMat(locS.x-i, locS.y+i)&&field[locS.x-i][locS.y+i]>-15) result.add(new Point(locS.x-i, locS.y+i));
-	    else break;
-	}
-	for(int i=1;i<MAT;i++){
-	    if(inMat(locS.x-i, locS.y-i)&&field[locS.x-i][locS.y-i]>-15) result.add(new Point(locS.x-i, locS.y-i));
-	    else break;
-	}
-
-	//		System.out.print("("+locS.x+","+locS.y+") == 1step ==> ");
-	//		System.out.println(result);
-	return result;
-    }
-
-    /**
-     * locSからlocGまで移動する際の最短ステップ数を返す
-     * @param locS スタート地点
-     * @param locG ゴール地点
-     */
-    private int bfs(Point locS, Point locG) {
-	boolean[][] visited = new boolean[MAT][MAT];
-	Queue<Point> q2 = new LinkedList<Point>(), q1;
-	q2.add(locS);
-	visited[locS.x][locS.y] = true;
-	int cnt = 0;
-	while(!q2.isEmpty()){
-	    //			System.out.println("cnt="+cnt +" ... "+q2);
-	    cnt++;
-	    q1 = null;		q1 = new LinkedList<Point>();
-	    for(Point p : q2) q1.add(p);
-	    q2 = null;		q2 = new LinkedList<Point>();
-	    while(!q1.isEmpty()){
-		List<Point> nextPoint = possibleMove(q1.poll());
-		for(Point p : nextPoint){
-		    if(!visited[p.x][p.y]){
-			if(p.x==locG.x&&p.y==locG.y){
-			    //							System.out.println("fin cnt="+cnt +" ... "+q1);
-			    //							System.out.println("("+locS.x+","+locS.y+") => ("+locG.x+","+locG.y+") = "+cnt);
-			    return cnt;
-			}
-			q2.add(p);
-			visited[p.x][p.y] = true;
-		    }
-		}
-	    }
-	}
-	return 20000;
-    }
-
-    private void input() {
 	Scanner stdIn = new Scanner(System.in);
-
 	int d = Integer.valueOf(stdIn.nextLine());
 	for(int z=0;z<d;z++){
 	    if(z!=0) stdIn.nextLine();
@@ -439,6 +267,7 @@ public class POJ2919 {
 		    if(line[j]=='Q'){
 			field[i][j] = QUEEN;
 			start = new Point(i, j);
+			start.beforeStr = start.getStr();
 		    }
 		    else if(line[j]=='P') field[i][j] = PAWN;
 		    else if(line[j]=='N'){
@@ -448,10 +277,11 @@ public class POJ2919 {
 		    else if(line[j]=='B') field[i][j] = BISHOP;
 		}
 	    }
-	    debug();
 	    standby();
-	    dp();
-	    output();
+	    // debug();
+	    debug2();
+	    System.out.println("Scenario #"+(z+1)+":");
+	    solve();
 	}
     }
 
@@ -465,11 +295,275 @@ public class POJ2919 {
 	System.out.println();
 	for(int i=0;i<MAT;i++){
 	    System.out.print(i+" |\t");
-	    for(int j=0;j<MAT;j++){
+	    for(int j=0;j<MAT;j++)
 		System.out.print(field[i][j]+"\t");
-	    }
 	    System.out.println();
 	}
-	System.out.println("===== === =====");
+	System.out.println("=== ===== ===");
+    }
+
+    private void debug2() {
+	System.out.println("=== debug2 ===");
+	System.out.print("\t");
+	System.out.println("a\tb\tc\td\te\tf\tg\th");
+	System.out.print("\t");
+	for(int i=0;i<MAT;i++) System.out.print("---\t");
+	System.out.println();
+	for(int i=0;i<MAT;i++){
+	    System.out.print((8-i)+" |\t");
+	    for(int j=0;j<MAT;j++)
+		System.out.print(field[i][j]+"\t");
+	    System.out.println();
+	}
+	System.out.println("=== ===== ===");
+    }
+
+    private void solve() {
+	memo = new String[1<<knightNum][MAT*MAT];
+	memoBFS = new String[MAT*MAT][MAT*MAT];
+	for(int i=0;i<(1<<knightNum);i++)
+	    Arrays.fill(memo[i], "z");
+	for(int i=0;i<MAT*MAT;i++)
+	    Arrays.fill(memoBFS[i], "z");
+
+	// TODO debug
+	// System.out.println(bfs(new Point(0, 7), new Point(7, 7)));
+	// System.exit(0);
+
+	long timeStart = System.currentTimeMillis();
+	f(0, start);
+	System.err.println((System.currentTimeMillis()-timeStart)+"msec");
+
+
+	int ans = memo[0][start.getIndex()].length()/2-1;
+	// System.out.println("ans="+ans+" , str="+memo[0][start.getIndex()]);
+
+	if(ans==-1)
+	    System.out.println("impossible"); else System.out.println(start.getStr()+memo[0][start.getIndex()]);
+	System.out.println();
+    }
+
+    private String f(int s, Point loc) {
+	// TODO debug
+	// padding(s);
+	// System.out.println("call : {"+Integer.toBinaryString(s)+"}\t"+loc);
+	if(memo[s][loc.getIndex()].compareTo("z")!=0){
+	    // TODO debug
+	    // padding(s);
+	    // System.out.println("\t return : {"+Integer.toBinaryString(s)+"}\t"+loc+"="+memo[s][loc.getIndex()]+" は一度計算 済");
+	    return memo[s][loc.getIndex()];
+	}
+	int ans = MAX_VALUE;
+	String ansStr = "z";
+	if(Integer.bitCount(s)==knightNum){
+	    for(Point p : neighboorBishop){
+		String temp = bfs(loc, p);
+		int temp_length = temp.length()/2;
+		if(temp.compareTo("z")==0) temp_length = MAX_VALUE;
+		if(ans>temp_length){
+		    // System.out.println("更新1 "+ansStr);
+		    ans = temp_length;
+		    ansStr = temp;
+		}
+		else if(ans==temp_length&&ansStr.compareTo(temp)>0){
+		    // System.out.println("更新2 "+ansStr);
+		    ansStr = temp;
+		}
+	    }
+	}
+	else{
+	    for(int k=0;k<knightNum;k++)
+		// iがsに含まれていないなら
+		if(((1<<k)&s)==0){
+		    int end = k*8+8;
+		    for(int nk=k*8;nk<end;nk++){
+			Point nei = neighboorKnight.get(nk);
+			// TODO 異変対応
+			if(!inMat(nei.x, nei.y)||field[nei.x][nei.y]<-15) continue;
+
+
+			String f_Str = f(s|(1<<k), nei);
+			if(f_Str.compareTo("z")==0) continue;
+
+			// TODO speed (これによりトラブル発生→わる2してなかった解決)
+			if(f_Str.length()/2>ans) continue;
+
+			String b_Str = bfs(loc, nei);
+			if(b_Str.compareTo("z")==0) continue;
+						
+			// TODO debug
+			// padding(s);
+			// System.out.println("bfs : "+loc+" => "+nei);
+
+
+			String now_Str = b_Str+f_Str;
+			// System.out.println("nowStr = "+now_Str);
+			int now_length = now_Str.length()/2;
+			// if(b_Str.compareTo("z")==0) continue;//now_length = MAX_VALUE;
+			if(ans>now_length){
+			    // System.out.println("更新3 "+ansStr);
+			    ans = now_length;
+			    ansStr = now_Str;
+			}
+			else if(ans==now_length&&ansStr.compareTo(now_Str)>0){
+			    // System.out.println("更新4 "+ansStr);
+			    ansStr = now_Str;
+			}
+		    }
+		}
+	}
+	memo[s][loc.getIndex()] = ansStr;
+	// TODO debug
+	// if(Integer.bitCount(s)==knightNum){
+	// TODO debug
+	// padding(ans);
+	// System.out.println("\t return : {"+Integer.toBinaryString(s)+"}\t"+loc+"="+memo[s][loc.getIndex()]+" の計算 完了");
+	// }
+	return ansStr;
+    }
+
+    private void padding(int s) {
+	for(int i=0;i<Integer.bitCount(s);i++)
+	    System.out.print("\t");
+    }
+
+    private String bfs(Point locS, Point locG) {
+	if(memoBFS[locS.getIndex()][locG.getIndex()].compareTo("z")!=0){
+
+	    // TODO debug
+	    // System.out.println("bfs memo ok : "+locS+","+locG+" => "+memoBFS[locS.getIndex()][locG.getIndex()]);
+
+	    return memoBFS[locS.getIndex()][locG.getIndex()];
+	}
+	else if(locS.x==locG.x&&locS.y==locG.y){
+	    memoBFS[locS.getIndex()][locG.getIndex()] = "";
+	    return memoBFS[locS.getIndex()][locG.getIndex()];
+	}
+
+	// TODO debug
+	// System.out.print("bfs memo ng : "+locS+" ==> "+locG+" ... ");
+
+
+	boolean[][] visited = new boolean[MAT][MAT];
+	Queue<Point> q2 = new PriorityQueue<Point>(), q1;
+	locS.beforeStr = "";
+	q2.add(locS);
+	int cnt = 0;
+	while(!q2.isEmpty()){
+	    cnt++;
+	    // System.out.println("cnt = "+cnt);
+	    q1 = null;	q1 = new PriorityQueue<Point>();
+	    for(Point p : q2){
+		visited[p.x][p.y] = true;
+		q1.add(p);
+	    }
+	    q2 = null;	q2 = new PriorityQueue<Point>();
+	    boolean finish_flag = false;
+	    while(!q1.isEmpty()){
+		Point now = q1.poll();
+		// TODO debug
+		// System.out.println("now "+now);
+		List<Point> nextPoint = possibleMove(now);
+		for(Point p : nextPoint){
+		    if(visited[p.x][p.y]) continue;
+		    // TODO debug
+		    // System.out.print(p+" ");
+		    String before_Str = memoBFS[locS.getIndex()][locG.getIndex()];
+		    String after_Str = now.beforeStr+p.getStr();
+		    if(p.x==locG.x&&p.y==locG.y){
+			finish_flag = true;
+			// System.out.println(before_Str+" ... "+after_Str);
+			// TODO 異変対応
+			if(before_Str.compareTo(after_Str)>0){
+			    // if(finish_flag){
+			    // System.out.println("更新");
+			    // }
+			    memoBFS[locS.getIndex()][locG.getIndex()] = after_Str;
+			}
+		    }
+		    p.append(after_Str);
+		    q2.add(p);
+		}
+	    }
+	    if(finish_flag){
+		// System.out.println(memoBFS[locS.getIndex()][locG.getIndex()]);
+		return memoBFS[locS.getIndex()][locG.getIndex()];
+	    }
+	}
+	memoBFS[locS.getIndex()][locG.getIndex()] = "z";
+	// System.out.println(memoBFS[locS.getIndex()][locG.getIndex()]);
+	return "z";
+    }
+
+    private List<Point> possibleMove(Point locS) {
+	// System.out.print(" "+locS+" ");
+	List<Point> result = new ArrayList<Point>();
+
+	// TODO debug 自分も加える
+	// if(field[locS.x][locS.y]>-15) result.add(locS);
+
+	for(int i=locS.x-1;i>=0;i--)
+	    if(field[i][locS.y]>-15) result.add(new Point(i, locS.y));
+	    else break;
+	for(int i=locS.x+1;i<MAT;i++)
+	    if(field[i][locS.y]>-15) result.add(new Point(i, locS.y));
+	    else break;
+	for(int i=locS.y-1;i>=0;i--)
+	    if(field[locS.x][i]>-15) result.add(new Point(locS.x, i));
+	    else break;
+	for(int i=locS.y+1;i<MAT;i++)
+	    if(field[locS.x][i]>-15) result.add(new Point(locS.x, i));
+	    else break;
+	for(int i=1;i<MAT;i++)
+	    if(inMat(locS.x+i, locS.y+i)&&field[locS.x+i][locS.y+i]>-15) result.add(new Point(locS.x+i, locS.y+i));
+	    else break;
+	for(int i=1;i<MAT;i++)
+	    if(inMat(locS.x+i, locS.y-i)&&field[locS.x+i][locS.y-i]>-15) result.add(new Point(locS.x+i, locS.y-i));
+	    else break;
+	for(int i=1;i<MAT;i++)
+	    if(inMat(locS.x-i, locS.y+i)&&field[locS.x-i][locS.y+i]>-15) result.add(new Point(locS.x-i, locS.y+i));
+	    else break;
+	for(int i=1;i<MAT;i++)
+	    if(inMat(locS.x-i, locS.y-i)&&field[locS.x-i][locS.y-i]>-15) result.add(new Point(locS.x-i, locS.y-i));
+	    else break;
+	return result;
+    }
+
+    /**
+     * KNIGHTとBISHOPの隣接リストの作成
+     */
+    private void standby() {
+	neighboorKnight = new ArrayList<Point>();
+	neighboorBishop = new ArrayList<Point>();
+	for(int i=0;i<MAT;i++){
+	    for(int j=0;j<MAT;j++)
+		if(field[i][j]==KNIGHT){
+		    for(int x=-1;x<2;x++)
+			for(int y=-1;y<2;y++)
+			    if(inMat(i+x, j+y)&&!(x==0&&y==0)&&field[i+x][j+y]!=PAWN)
+				neighboorKnight.add(new Point(i+x, j+y));
+			    else{
+				if(x==0&&y==0) continue;
+				neighboorKnight.add(new Point(-1,-1));
+			    }
+		}
+		else if(field[i][j]==BISHOP){
+		    for(int x=-1;x<2;x++)
+			for(int y=-1;y<2;y++)
+			    if(inMat(i+x, j+y)&&!(x==0&&y==0))
+				neighboorBishop.add(new Point(i+x, j+y));
+		}
+	}
+    }
+
+    /**
+     * マップ内であるか判定する
+     * @param i
+     * @param j
+     * @return マップ内である
+     */
+    private boolean inMat(int i, int j) {
+	if(i>=0&&i<MAT&&j>=0&&j<MAT) return true;
+	return false;
     }
 }
